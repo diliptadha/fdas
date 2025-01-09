787f02100a178789b83064793a10077b24d912b1
diliptadha.dev <diliptadha.dev@gmail.com>
2025-01-09 11:14:57 +0000

    ok

---
+++ index.ts
@@ -0,0 +1,29 @@
+import { S3Client, s3 } from "bun";
+
+const client = new S3Client({
+  accessKeyId: "env",
+  secretAccessKey: "T+/env/aF13TB3FhHsS",
+  bucket: "demo-hai-bro",
+  endpoint: "https://storage.googleapis.com",
+  // acl: "public-read",
+});
+
+const response = await fetch(
+  "https://storage.googleapis.com/demo-hai-bro/demo/20241215111916_3dicons-png-dynamic-1.0.0.zip"
+);
+
+const lazyS3File = client.file("asdf");
+
+const resposes = new Response(lazyS3File);
+console.log(resposes);
+
+// const writer = lazyS3File.writer({
+//   retry: 3,
+//   queueSize: 10,
+//   type: "application/octet-stream",
+//   partSize: 5 * 1024 * 1024,
+// });
+// for await (const chunk of response.body) {
+//   writer.write(chunk);
+// }
+// await writer.end();
